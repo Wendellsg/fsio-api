88197d0f83d38cceaf5054aa06b9c340ecaf2dd4
Wendell Guimarães <wendell.silva.guimaraes@gmail.com>
2024-07-10 22:34:50 -0300

    feat: professional creation

--- src/professionals/professionals.controller.ts
+++ src/professionals/professionals.controller.ts
@@ -1,11 +1,33 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
-import { UserRoleEnum } from '@prisma/client';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { Prisma, UserRoleEnum } from '@prisma/client';
 import { AuthGuard, Roles } from 'src/auth/auth.guard';
 import { ProfessionalsService } from './professionals.service';
 
 @Controller('professionals')
 export class ProfessionalsController {
   constructor(private readonly professionalsService: ProfessionalsService) {}
+
+  @Roles(UserRoleEnum.patient)
+  @UseGuards(AuthGuard)
+  @Post('')
+  create(@Request() request, @Body() body: Prisma.ProfessionalCreateInput) {
+    return this.professionalsService.create(request.user.id, body);
+  }
+
+  @Roles(UserRoleEnum.professional)
+  @UseGuards(AuthGuard)
+  @Patch(':id')
+  update(@Request() request, @Body() body: Prisma.ProfessionalCreateInput) {
+    return this.professionalsService.update(request.user.id, body);
+  }
 
   @Roles(UserRoleEnum.patient)
   @UseGuards(AuthGuard)
@@ -18,6 +40,9 @@ export class ProfessionalsController {
   @UseGuards(AuthGuard)
   @Get('/me')
   me(@Request() request) {
-    return this.professionalsService.me(request.user.professionalId);
+    return this.professionalsService.me(
+      request.user.professionalId,
+      request.user.id,
+    );
   }
 }
--- src/professionals/professionals.service.ts
+++ src/professionals/professionals.service.ts
@@ -1,9 +1,36 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProfessionalsService {
   constructor(private prisma: PrismaService) {}
+
+  async create(userId: string, data: Prisma.ProfessionalCreateInput) {
+    const professional = await this.prisma.professional.create({
+      data: {
+        ...data,
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+
+    return professional;
+  }
+
+  async update(userId: string, data: Prisma.ProfessionalUpdateInput) {
+    const professional = await this.prisma.professional.update({
+      where: {
+        userId: userId,
+      },
+      data,
+    });
+
+    return professional;
+  }
 
   async findAll(id: string) {
     const professionals = await this.prisma.professional.findMany({
@@ -31,20 +58,10 @@ export class ProfessionalsService {
     return professionals;
   }
 
-  async me(id: string) {
+  async me(id: string, userId: string) {
     const professional = await this.prisma.professional.findFirst({
       where: {
-        id: id,
-      },
-      select: {
-        id: true,
-        user: {
-          select: {
-            name: true,
-            email: true,
-            image: true,
-          },
-        },
+        userId,
       },
     });
 
